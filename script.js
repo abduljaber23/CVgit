fdf67a973c1a8bda131b4a1d9ae0e9aed452b328
Al omran abduljaber <alomranabduljaber@gmail.com>
2025-01-12 00:45:38 +0100

    atouts infos

--- script.js
+++
@@ -1,7 +0,0 @@
-const titre = document.querySelector('h1');
-const items = document.querySelectorAll('li');
-const text = document.getElementsByClassName('text');
-const title = document.getElementById('title');
-const el = document.getElementsByTagName('a');
-console.log(titre , items , text , title , el);
-alert
